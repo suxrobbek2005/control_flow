885469a668865f2a9c8b2b60f4d84757854d9fa1
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 06:22:09 -0500

    Done

---
+++ 01.py
@@ -0,0 +1,9 @@
+# Dasturimizga foylananuchi malumot kiritishi uchun
+word = input("Biror bir matn kiritining ")
+
+#Kiritilgn matni katta harflarga uzgartilish shartini quydik
+to_upper = word.upper()
+
+# Bajarilgan ammallar terminalga chiqarildi
+print("Kiritilgan matn katta harflarga o'zgartirildi")
+print (to_upper)
