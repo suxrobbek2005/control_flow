0941a5c7bbcb29bdc5c25d30e6a3ebdc9bafb35e
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 08:20:49 -0500

    Done'

---
+++ 09.py
@@ -0,0 +1,6 @@
+# Dasturmizga foylananuvchi malumot kiritishini taminladik
+number = int (input ("Biror bir raqam kiriting = "))
+
+# Dasturmizga kirtilgan sondan boshlab hisoblandi 
+for i in range(0, number):
+    print(i)
