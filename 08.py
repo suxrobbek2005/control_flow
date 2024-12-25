6e0cc877dce4d177423da1bbcb0cbbde007c16a4
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 08:14:22 -0500

    Done

---
+++ 08.py
@@ -0,0 +1,6 @@
+# Dasturga foydalanuchidan malumot kiritshi taminlandi
+word = input ("Biror bir matn kiritining ")
+
+# Kiritilgan malumotlar qatori o'zgartirildi 
+for i in word.split():  # Xar bir so'z xisoblandi
+    print(i)
