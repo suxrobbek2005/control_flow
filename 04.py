d387276f63e387c98788336603d771472c932266
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 06:57:29 -0500

    Done

---
+++ 04.py
@@ -0,0 +1,9 @@
+# Dasturimizga foylanuvchi tamonida malumot kiritadi
+number = int (input("Yoshingizni kiritining "))
+
+# Foydalnauchini voyaga yetganligi haqida maluot beradi
+if number >= 18:
+    print ("Siz voyaga yetgansiz ")
+else  :
+    print ("Siz voyaga yitmagansiz")
+
