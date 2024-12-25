44902b7b52cb5f2301d99cfc85fd7918ba1b0fc3
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 07:23:16 -0500

    Done

---
+++ 06.py
@@ -0,0 +1,9 @@
+# Dasturmizga foydalananuchi maluot kiritdi
+word = str (input("Biror bir matn kiritinig "))
+
+# Foydalanuvchi kiritgan matni bosh harflarni katta harfga o'zgartirildi
+new_word = word.title()
+
+# bajarilgan amallarni terminlaga chiqarishini suradik
+print("Siz kiritgan matn bosh harflari katta harfga o'zgartirildi ")
+print (new_word)
