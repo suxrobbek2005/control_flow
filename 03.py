cc9d2012f756c7e419762674286368b599711452
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 06:51:29 -0500

    Done

---
+++ 03.py
@@ -0,0 +1,9 @@
+# Dasturmizga malumot kirishini taminlab oldik
+word = input ("Biror bir matn kiritining ")
+search = input ("Kiritgan matndan qaysi harfni topish kerak ")
+
+letter_count =  word.count(search)
+formated = f"Siz kiritgan matnda {letter_count}"
+
+
+print(formated)
