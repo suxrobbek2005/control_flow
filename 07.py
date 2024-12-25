b73605b026b35dc97a8f6b069ff661a2f55c322b
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 07:59:20 -0500

    Done

---
+++ 07.py
@@ -0,0 +1,9 @@
+#Dasturga foydalanuvchidan malumot kiritshini taminladik
+word =  input ("Biror bir matn kiritining \n")
+search = str (input ("Izlayitgan so'zingizni kiriting "))
+
+# Kiritilgan matnda borligini aniqladi
+search_word = search in word 
+
+# Bajarilgan amallar terminlaga chiqishini taminlab oldik
+print (search_word)
