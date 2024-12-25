2684a895a228b7d75c224e61a1f048eedb63e322
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-25 07:16:40 -0500

    Done -

---
+++ 05.py
@@ -0,0 +1,9 @@
+# Dasturimizga foydalunachi malumot kiritishini suraymiz
+word = str(input("Biror bir matn kiritining "))
+
+# Kiritilgan malumot for bilan va range funksiyasi orqali 3 marta aylantirildi
+for i  in range(3):
+    print(word)
+    
+
+
